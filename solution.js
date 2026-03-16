0cfb7ecff004ea7ad1e1f317fd94274fdfdcb884
escalarencey <escala.rence03@gmail.com>
2026-03-16 21:33:36 +0800

    Solve Problem 5

--- solution.js
+++ solution.js
@@ -1,7 +1,11 @@
-// Problem 4: Leaderboard Filter
-function getTopScorers(playerList) {
-    return playerList
-        .filter(player => player.score > 8)
-        .map(player => player.name)
-        .join(", ");
+// Problem 5: The Private Inventory
+class Item {
+    #discount = 0.1; 
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    get finalPrice() {
+        return this.price - (this.price * this.#discount);
+    }
 }
